41a4216ab24cca41ad68b654bb293a64908974c0
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-11-18 09:13:59 -0500

    Document the function with JSDoc

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const groupBy = require("lodash.groupby");
 
+/**
+ * Group an array alphabetically.
+ * @param {*[]} input An array of stringifiable values.
+ */
 function alphabeticalGroup(input = []) {
 	return groupBy(input.sort(), item => {
 		return item.toString()[0].toLowerCase();
